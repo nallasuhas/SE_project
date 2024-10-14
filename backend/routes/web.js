babe2e340a5ccbedcb97fa89501af9d802158ced
nallasuhas <suhasnalla43@gmail.com>
2024-10-14 22:20:21 +0530

    added cart logic

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -1,5 +1,6 @@
 const {postRegister, register, postLogin, login, logout} = require('../controllers/AuthControllers')
 const express = require('express');
+const { index, updateCart } = require('../controllers/CartControllers');
 const router = express.Router()
 
 
@@ -11,5 +12,8 @@ router.post('/register', postRegister)
 router.get('/login', login)
 router.post('/login', postLogin)
 router.post('/logout', logout )
+
+router.get('/cart', index)
+router.post('/update-cart', updateCart )
 
 module.exports = router
