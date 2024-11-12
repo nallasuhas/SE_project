1b5b55eaa4aab0f2efbb8d0c7c9ac70778db183c
nallasuhas <suhasnalla43@gmail.com>
2024-11-12 22:02:43 +0530

    resolved

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,16 +1,10 @@
 const Menu = require('../models/menu')
-<<<<<<< HEAD
-=======
+
 // /menus implementing this logic 
 async function menu(req,res){
     const pizzas = await Menu.find()
     return res.render("menus", {pizzas: pizzas})
 }  
 module.exports= {menu}
->>>>>>> main
 
-async function menu(req, res){
-    const pizzas = await Menu.find()
-    return res.render('menu', {pizzas: pizzas})
 
-}module.exports = {menu}
