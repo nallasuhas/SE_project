bbdb2b1947d59baa84e6c729d8ae38ed4ca88422
Pravallika Kotte <146621306+prav-kotte1@users.noreply.github.com>
2024-11-12 17:21:34 +0530

    Modified CSS for auth and home pages

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,8 +1,7 @@
 const Menu = require('../models/menu')
-// /menus implementing this logic 
-async function menu(req,res){
-    const pizzas = await Menu.find()
-    return res.render("menus",{pizzas: pizzas})
-}
-module.exports={menu}
 
+async function menu(req, res){
+    const pizzas = await Menu.find()
+    return res.render('menu', {pizzas: pizzas})
+
+}module.exports = {menu}
